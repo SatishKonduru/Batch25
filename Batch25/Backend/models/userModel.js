b87e843d751cae40423607ffbab72c207fdd6d87
satish <satishkonduru0225@gmail.com>
2024-04-20 07:05:23 +0530

    userModel and routes configured in Frontend

--- Batch25/Backend/models/userModel.js
+++ Batch25/Backend/models/userModel.js
@@ -43,6 +43,7 @@ const userSchema = new mongoose.Schema({
     role: {
         type: String,
         default: 'user'
+        
     },
     
     image: {
--- Batch25/Frontend/src/app/app.routes.ts
+++ Batch25/Frontend/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
     {
@@ -13,8 +15,21 @@ export const routes: Routes = [
         component: HomeComponent
     },
     {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+
+
+
+
+    
+    {
         path: '**',
         component: PageNotFoundComponent
      },
-
+   
 ];
