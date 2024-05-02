e86ef4fc54d465a9236c515fc8dc934af2e4ccd1
satish <satishkonduru0225@gmail.com>
2024-05-02 06:35:12 +0530

    Working on ProductAPI

--- Batch25/Backend/routes/productAPI.js
+++ Batch25/Backend/routes/productAPI.js
@@ -5,6 +5,7 @@ const {Product} = require('../models/productModel')
 const {authenticateToken} = require('../AuthServices/authentication')
 const {checkRole} = require('../AuthServices/checkRole')
  const {Category} = require('../models/categoryModel')
+ const mongoose = require('mongoose')
 
 
 //Getting all Products
@@ -77,7 +78,104 @@ router.get('/getById/:id', authenticateToken, async (req, res) => {
 })
 
 
+//update product by id
+router.patch('/update/:id', authenticateToken, checkRole,  async (req, res) => {
+    const productId = req.params.id
+    const newData = req.body
+    if(!mongoose.isValidObjectId(productId)){
+        return res.status(500).send({
+            message: 'Invalid Object Id'
+        })
+    }
+
+    updateProduct = await Product.findByIdAndUpdate(productId, {name: newData.name}, {new: true})
+    if(!updateProduct){
+        return res.status(500).send({
+            message: 'Invalid Product Selection'
+        })
+    }
+    else{
+        return res.status(200).send({
+            message: 'Product Updated Successfully',
+            newData: updateProduct
+        })
+    }
+
+})
 
 
+//Delete Product
+router.delete('/delete/:id',authenticateToken, checkRole, async (req, res)=>{ 
+    const productId = req.params.id
+    if(!mongoose.isValidObjectId(productId)){
+        return res.status(500).send({
+            message: 'Invalid Object Id'
+        })
+    }
+
+    deleteProduct = await Product.findByIdAndDelete(productId)
+    if(!deleteProduct){
+        return res.status(500).send({
+            message: 'No Produt was Found'
+        })
+    }
+    else{
+        return res.status(200).send({
+            message: 'Product Deleted Successfully.'
+        })
+    }
+
+})
+
+
+//Getting product Count
+router.get('/getCount',authenticateToken, checkRole,  async (req, res) => {
+    productCount = await Product.countDocuments()
+    if(!productCount){
+        return res.status(500).send({
+            message: 'No Products were found'
+        })
+    }
+    else{
+        return res.status(200).send({
+            count: productCount
+        })
+    }
+})
+
+
+//Get Products by Category
+router.get('/ByCategory/:id', async (req, res) => {
+    const categoryId = req.params.id
+    productDetails = await Product.find({category: categoryId}).populate('category')
+    if(productDetails.length <= 0 ){
+        return res.status(500).send({
+            message: 'No Products were found in given Category'
+        })
+    }
+    else{
+        return res.status(200).send({
+            products: productDetails
+        })
+    }
+    
+})
+
+//get products count by category
+
+router.get('/getCountByCategory/:id', authenticateToken, checkRole, async (req, res) => {
+    const categoryId = req.params.id
+    productCount = await Product.find({category: categoryId}).countDocuments()
+    if(!productCount){
+        return res.status(500).send({
+            message: "No Products were found in given Category"
+        })
+    }
+    else{
+        return res.status(200).send({
+            productCount: productCount
+        })
+    }
+})
 
 module.exports = router
