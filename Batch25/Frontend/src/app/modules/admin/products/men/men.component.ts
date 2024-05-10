7d45ed3d07a6418764a29b86abfa8b0b855599b9
satish <satishkonduru0225@gmail.com>
2024-05-10 06:29:37 +0530

    Discussion about future developement

--- Batch25/Frontend/src/app/modules/admin/products/men/men.component.ts
+++ Batch25/Frontend/src/app/modules/admin/products/men/men.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { AngularMaterialModule } from '../../../angular-material/angular-material.module';
+import { ProductCardComponent } from '../../shared/product-card/product-card.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AngularEditorModule } from '@kolkov/angular-editor';
+import { SnackbarService } from '../../../../services/snackbar.service';
+import { MenService } from '../../men/men.service';
 
 @Component({
   selector: 'men',
   standalone: true,
-  imports: [],
+  imports: [
+    AngularMaterialModule, 
+    ProductCardComponent,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AngularEditorModule
+  ],
   templateUrl: './men.component.html',
-  styleUrl: './men.component.css'
+  styleUrl: './men.component.css',
+  providers: [SnackbarService, MenService ]
 })
 export class MenComponent {
 
