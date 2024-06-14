dc04fa2c9718a6f144922d8e16be86376d3709a3
satish <satishkonduru0225@gmail.com>
2024-06-14 07:32:42 +0530

    Working on Charts

--- Batch25/Frontend/src/app/modules/admin/dashboard/dashboard.component.ts
+++ Batch25/Frontend/src/app/modules/admin/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, inject } from '@angular/core';
 import { AngularMaterialModule } from '../../angular-material/angular-material.module';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import * as echarts from 'echarts';
 import { Observable, map } from 'rxjs';
 import { DashboardService } from './dashboard.service';
+import { Title } from '@angular/platform-browser';
 
 
 
@@ -18,6 +19,8 @@ import { DashboardService } from './dashboard.service';
 })
 
 export class DashboardComponent implements OnInit{
+  @ViewChild('PieContainer', {static: true}) PieContainer : ElementRef;
+  @ViewChild('LineContainer', {static: true}) LineContainer : ElementRef;
   count$ : Observable<any>
   dashboardService = inject(DashboardService)
   products: any[] = []
@@ -33,7 +36,66 @@ export class DashboardComponent implements OnInit{
                                       .pipe(
                                             map((res) => res.count)
                                           )
+  this.dashboardService.getProducts().subscribe((data: any) => {
+    if(data.products){
+      this.products = data.products
+      this.countCategories()
+      this.renderPieChart()
+      // this.renderLineChart()
+
+    }
+  })
+  }    
+  countCategories(){
+    this.menCount = this.products.filter(p => p.category.name === 'Men')
+                    .reduce((total, p) => total+p.countInStock , 0)
+
+   this.womenCount = this.products.filter(p => p.category.name === 'Women')
+                    .reduce((total, p) => total+p.countInStock , 0) 
+
+  this.kidsCount = this.products.filter(p => p.category.name === 'Kids')
+                    .reduce((total, p) => total+p.countInStock , 0)
   }
 
+  renderPieChart(){
+    const echartsElement : HTMLElement = this.PieContainer.nativeElement
+    if(!echartsElement){
+      console.log("Pie chart containter is not Available")
+      return
+    }
+    const myChart = echarts.init(echartsElement)
+    const option = {
+      title: {
+        text: 'Categories',
+        left: 'center',
+      },
+      tooltip: {
+        trigger: 'item'
+      },
+      series:{
+        name: 'Category of..',
+        type: 'pie',
+        roseType: 'radius',
+        radius: '85%',
+        data: [
+          {value: this.menCount, name: 'Men'},
+          {value: this.womenCount, name: 'Women'},
+          {value: this.kidsCount, name: 'Kids'}
+        ],
+        emphasis: {
+          itemStyle : {
+            shadowBlur: 20,
+            shadowOffsetX: 0,
+            shadowColor: '#000'
+          },
+          color: ['#ff6666', '#009933', '#890b3ff']
+        }
+      }
+    }
 
+    myChart.setOption(option)
+    myChart.on('error', function(error){console.error('ECharts error: ', error)})
+  }
+
+  renderLineChart(){}
 }
