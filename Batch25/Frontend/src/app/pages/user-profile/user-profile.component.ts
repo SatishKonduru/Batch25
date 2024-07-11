4fa16fac36467a3d4eb4810af31520fb601ff3ce
satish <satishkonduru0225@gmail.com>
2024-07-11 09:57:44 +0530

    woriking on Profile

--- Batch25/Frontend/src/app/pages/user-profile/user-profile.component.ts
+++ Batch25/Frontend/src/app/pages/user-profile/user-profile.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, inject } from '@angular/core';
 import { AngularMaterialModule } from '../../modules/angular-material/angular-material.module';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { UserService } from '../../services/user.service';
@@ -52,5 +52,76 @@ export class UserProfileComponent implements OnInit{
       shareReplay()
     )
   }
+
+  @ViewChild('fileInput') fileInput : ElementRef;
+  @ViewChild('fileInputFiled') fileInputField : ElementRef
+
+  image : any
+  imageSelected: boolean = false
+
+  onFileSelected(event: any):void {
+    const fileInput = event.target
+    if(fileInput.files && fileInput.files.length > 0){
+      const file: File = fileInput.files[0]
+      this.image = file
+      this.previewImage(file)
+      this.fileInputField.nativeElement.value = file.name
+      this.imageSelected = true
+    }
+  }
+
+  openFileInput():void{
+    this.fileInput.nativeElement.click()
+  }
+
+  selectedFileName : string | undefined;
+  selectedImage : string | undefined;
+
+previewImage(file: File){
+  const reader = new FileReader()
+  reader.onload = (e) => {
+    this.selectedImage = e.target?.result as string
+  }
+  reader.readAsDataURL(file)
+}
+
+
+updateUser(){
+  const userDetails = this.userForm.value
+  const imageFile = this.image
+  const formData = new FormData()
+  formData.append('name', userDetails.name)
+  formData.append('email', userDetails.email)
+  formData.append('phone', userDetails.phone)
+  formData.append('apartment', userDetails.apartment)
+  formData.append('street', userDetails.street)
+  formData.append('city', userDetails.city)
+  formData.append('country', userDetails.country)
+  formData.append('image', imageFile)
+
+  this.userService.updateUser(this.userId, formData).subscribe({
+    next: (res: any) => {
+      if(res?.message){
+        this.responseMsg = res?.message
+        this.getUserDetails(this.userId)
+        this.snackbar.openSnackbar(this.responseMsg,'success')
+      }
+    },
+    error: (err: any) => {
+      if(err.error?.message){
+        this.responseMsg = err.error?.message
+      }
+      else{
+        this.responseMsg = globalProperties.genericError
+      }
+      this.snackbar.openSnackbar(this.responseMsg, globalProperties.error)
+    }
+  })
+}
+
+
+
+
+
 
 }
