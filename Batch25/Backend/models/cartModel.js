386fc5b487e0649167ede47d61125151a26c88b4
satish <satishkonduru0225@gmail.com>
2024-07-15 19:53:59 +0530

    added cartModel and wishlistModel

---
+++ Batch25/Backend/models/cartModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const {User} = require('./userModel')
+const {Product} = require('./productModel')
+
+const cartSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    items: [
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Product'
+            },
+            quantity: Number
+        }
+    ],
+    totalPrice: {
+        type: Number,
+        default: 0
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now
+    },
+    status: {
+        type: String,
+        enum: ['Active', 'Expired','Completed'],
+        default: 'Active'
+    }
+})
+cartSchema.virtual('id').get(function() {
+    return this._id.toHexString()
+})
+cartSchema.set('toJSON', {virtuals: true})
+const Cart = mongoose.model('Cart', cartSchema,'carts')
+module.exports = { Wishlist: Wishlist}
